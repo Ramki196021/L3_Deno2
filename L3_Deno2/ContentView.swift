6ce5968a9e20ec145b6f58ae48e1728b440e56a5
Ramki <flash_zoro@yahoo.com>
2024-05-14 16:08:32 +0800

    Initial Commit

---
+++ L3_Deno2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  L3_Deno2
+//
+//  Created by Ramakrishnan K on 14/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ L3_Deno2/L3_Deno2App.swift
@@ -0,0 +1,17 @@
+//
+//  L3_Deno2App.swift
+//  L3_Deno2
+//
+//  Created by Ramakrishnan K on 14/5/24.
+//
+
+import SwiftUI
+
+@main
+struct L3_Deno2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
