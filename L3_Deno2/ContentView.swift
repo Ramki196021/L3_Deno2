3a7c93e19ab065f478da1ad23ac5c09ccb8de7c0
Ramki <flash_zoro@yahoo.com>
2024-05-15 23:00:26 +0800

    commit 15/5

--- L3_Deno2/ContentView.swift
+++ L3_Deno2/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      
+        VStack(alignment: .leading, spacing: 20.0) {
+            Image("tulipmania-SharpenAI-Motion")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+            
+            HStack{
+                Text("Garden by the Bay")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Spacer()
+                VStack{
+                    HStack{
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                    }
+                    Text("(Reviews 32")
+                }
+                .foregroundColor(.orange)
+                    .font(.caption)
+            }
+            
+        
+                Text("You find Nice Tulip flowers in the Garden this month")
+            HStack {
+                Spacer()
+                Image(systemName:"binoculars.fill")
+                Image(systemName: "fork.knife")
+                    .padding(12)
+            }
+            .foregroundColor(.gray)
+                .font(.caption)
+            
         }
         .padding()
+        .background(Rectangle().foregroundColor(.green))
+        .padding()
+        
     }
 }
 
